150302a980cd3107574b5542483ebcd716d1a41e
KizunaKiseki <74109635+KizunaKiseki@users.noreply.github.com>
2026-04-16 14:28:56 -0500

    Added global variables for walking mechanism

--- KinematicAnalysis.py
+++ KinematicAnalysis.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-TITLE = Kinematic Analysis Program
+TITLE = Kinematic Analysis Program ~ General Planar Motion
 DATE  = 2026.04.16
 _____________________________________________________________________
 DESCRIPTION:
-1. [Insert Description Here]
-2. ...
-_____________________________________________________________________
-HOW-TO:
--> Click on the button
--> ...
-_____________________________________________________________________
-UPDATES:
-- [2026.02.02] - 1.1 ...
-- [2026.01.01] - 1.0 RELEASE
-_____________________________________________________________________
-TO-DO:
-- Describe Next Features
-- ...
+1. Kinematic analysis program to determine velocities and accelerations of the pins.
+2. The program will use general planer equations to determine the velocities and accelerations of the pins.
+3. The program will display the results through a graphical representation of the mechanism and a table of the results.
 _____________________________________________________________________
 AUTHOR : Nicholas Heling
 """
@@ -26,7 +15,7 @@ AUTHOR : Nicholas Heling
 # ? ================================================================ ?
 
 # ! PYTHON TEMPLATES & LIBRARIES !
-
+import numpy as np
 
 # ! PROJECT MODULES !
 
@@ -34,6 +23,27 @@ AUTHOR : Nicholas Heling
 # * VARIABLES *
 # ? ================================================================ ?
 
+# LINK DIMENSIONS
+# * ALL LINK DIMENSIONS ARE IN MM *
+LINK_A_LENGTH = 57.00       
+LINK_B_LENGTH = 62.25
+LINK_C_LENGTH = 58.95
+LINK_D_LENGTH = 60.15
+LINK_E_LENGTH = 83.70
+LINK_F_LENGTH = 59.10
+LINK_G_LENGTH = 55.05
+LINK_H_LENGTH = 98.55
+LINK_I_LENGTH = 73.50
+LINK_J_LENGTH = 75.00
+LINK_K_LENGTH = 92.85
+LINK_L_LENGTH = 11.70
+LINK_M_LENGTH = 22.50
+
+# MOTOR PARAMETERS
+# * MOTOR REVOLUTION IS IN RPM *
+# * MOTOR ANGULAR VELOCITY IS IN RAD/S *
+MOTOR_REVOLUTION = 30.00                                           
+MOTOR_ANGULAR_VELOCITY = (MOTOR_REVOLUTION * 2 * np.pi) / 60.00     
 
 
 # * FUNCTION *
